cf0e8f2092dec775b6a013b14ec6429da1dda953
alguienquenose1010-ai <alguienquenose1010@gmail.com>
2026-05-15 20:56:44 -0300

    con error

---
+++ funciones_botones.py
@@ -0,0 +1,85 @@
+import flet as ft, chess, chess.svg
+
+def mostrar_inicio(page):
+    page.controls.clear()
+    text = ft.Text(
+        "Bienvenido al juego de ajedrez.",
+        size=30,
+        weight=ft.FontWeight.BOLD,
+        color=ft.Colors.RED,
+    )
+    image = ft.Image(
+        src="https://flet.dev/img/logo.svg", 
+        width=100, 
+        height=100
+    )
+
+    button1 = ft.ElevatedButton(
+        content=ft.Text("JUGAR"),
+        bgcolor=ft.Colors.GREEN,
+        color=ft.Colors.WHITE,
+        on_click=lambda e: jugar(page)
+    )
+    button2 = ft.ElevatedButton(
+        content=ft.Text("VER GANADORES DE PARTIDAS ANTERIORES"),
+        bgcolor=ft.Colors.GREEN,
+        color=ft.Colors.WHITE,
+        on_click=lambda e: ver_ganadores(page)
+    )
+
+    page.add(text, image, button1, button2)
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
+
+def jugar(page):
+    page.controls.clear()
+
+    jugador1 = ft.TextField(hint_text="Nombre del jugador 1")
+    jugador2 = ft.TextField(hint_text="Nombre del jugador 2")
+
+    page.add(
+        jugador1,
+        jugador2,
+        ft.ElevatedButton(content=ft.Text("JUGAR"), on_click=lambda e: JUGAR(page)),
+        ft.ElevatedButton(content=ft.Text("Volver"), on_click=lambda e: mostrar_inicio(page))
+    )
+
+    page.update()
+
+def ver_ganadores(page):
+    page.controls.clear()
+    try:
+        with open("ganadores.txt", "r") as f:
+            lineas = f.readlines()
+        if lineas:
+            for linea in lineas:
+                page.add(ft.Text(linea.strip()))
+        else:
+            page.add(ft.Text("Todavía no hay ganadores."))
+    except FileNotFoundError:
+        page.add(ft.Text("El archivo de ganadores no existe."))
+
+    page.add(ft.ElevatedButton(content=ft.Text("Volver"), on_click=lambda e: mostrar_inicio(page)))
+    page.update()
+
+
+def JUGAR(page):
+    page.controls.clear()
+
+    board = chess.Board()
+    svg_code = chess.svg.board(board)
+
+    # Mostrar el SVG directamente
+    page.add(
+        ft.Html(
+            content=svg_code,
+            width=600,
+            height=600
+        )
+    )
+
+    page.add(ft.ElevatedButton(content=ft.Text("Volver"), on_click=lambda e: mostrar_inicio(page)))
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.update()
---
+++ main.py
@@ -0,0 +1,7 @@
+import flet as ft
+import funciones_botones as fb
+
+def main(page: ft.Page):
+    fb.mostrar_inicio(page)
+
+ft.run(main)
